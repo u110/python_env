b40aa654ef4d6ea8f62ad5fc288d437b8e26a5a5
u110 <yuu.aizu@gmail.com>
2018-04-17 16:18:05 +0900

    add setup.py template

--- Makefile
+++ Makefile
@@ -8,3 +8,9 @@ env:
 
 pip:
 	env/bin/pip install -r requirements.txt
+
+install_dev: setup.py
+	env/bin/pip install -e .
+
+setup.py:
+	cp templates/setup.py setup.py
---
+++ templates/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='sample_module_name',
+      description='sample module name',
+      author="Yuu Ito",
+      author_email="yuu.aizu@gmail.com",
+      version='0.1',
+)
